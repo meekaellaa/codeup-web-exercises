6082cadaf4c3196b6bd095bd40f8230eb98ff988
Michaella Moreno <michaella.moreno@gmail.com>
2023-04-14 09:34:49 -0500

    Added 2 colors, pink an yellow.

--- js/external.js
+++ js/external.js
@@ -1,1 +1,16 @@
 console.log("Whats UP Dragon!");
+
+
+
+
+//alert("Hey Dragon.");
+
+
+// the following line will show the OK/CANCEL confirm dialog
+//var confirmed = confirm('Are you sure you want to do XYZ?');
+//console.log(confirmed); // will be either true or false
+
+
+var userInput = prompt('Please type something:');
+console.log('The user entered: ' + userInput);
+
