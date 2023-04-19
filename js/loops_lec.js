30423cd694f72e12ea6fc395fc71f089e19491d7
Michaella Moreno <michaella.moreno@gmail.com>
2023-04-19 14:49:25 -0500

    added more examples

--- js/loops_lec.js
+++ js/loops_lec.js
@@ -45,6 +45,9 @@ function areWeThereYetNow(){
         companyHasEmail = confirm("Please, can we have your email?");
     } while ( companyHasEmail === false)
 
+
+
+
 // commonly used at codeup: for loop, separation of different elements use (;)
 // inside these parentheses have 3 elements. initialization : idirator known as i
 // conditions: arithmetic value, reassignment of the
@@ -62,4 +65,60 @@ for (let i = 10; i > 0; i-=2 ){
     console.log(i);
 }
 
+
+
+
+while(true){
+    if(confirm("Are we there yet?????")){
+  //break; will break me out of my loop!
+        break;
+    }
+}
+alert("FINALLY! WE HAVE ARRIVED TO JAMAICA");
+// continue works as a fast-forward/skip button.
+for (let i = 10; i > 0; i--){
+    if (i % 2 !== 0){
+// takes advantage of the continue.
+        console.log("OOOOPS... EXTREMELY ODD NUMBER.");
+        continue;
+    }
+    console.log(i);
+}
+//
+
 // })();
+
+
+
+// While Loop
+let count = 1;
+while(count < 10){
+    console.log((count));
+    count++;
+}
+// loop will stop because 1,2,3,4,5,6,7,8,9 is true but 10 is no longer true.
+
+// do while loop
+// let sum = 5;
+// do{
+//     console.log(sum);
+//     sum += 5;
+// }
+// while(sum !== 5);
+// created an infinite loop.
+//example:
+let sum = 5;
+do{
+    console.log(sum);
+    sum += 5;
+}
+while(sum < 56);
+
+//pimp my ride behavior....
+for (let i = 1; i < 11; i++){
+    // make sure you don't use i here again.
+    for (let m = 1; m < 11; m ++){
+        console.log(`${i} + ${m} = ${i + m}`);
+    }
+}
+
