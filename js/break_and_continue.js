726ad32b948c794820fe431f69542bdbadb68e32
Michaella Moreno <michaella.moreno@gmail.com>
2023-04-20 15:44:15 -0500

    added David's solutions

--- js/break_and_continue.js
+++ js/break_and_continue.js
@@ -24,5 +24,24 @@ for (let i = 1; i <= 49; i++){
         console.log('Here is an ODD number ' + i);
 }
 
+//David's Solution:
+
+let numToSkip;
+    while (true){
+        numToSkip = parseInt(prompt("Give me a number between 1 and 50, NOW."))
+       if(numToSkip >= 1 && numToSkip <= 50 && numToSkip % 2 !== 0){
+           break;
+        }
+    }
+
+    for(let i = 1; i < 50; i += 2){
+        if( i === numToSkip){
+            console.log(`OOOOPS! SKIPING ${numToSkip}`);
+            continue;
+        }
+        console.log(i);
+    }
+
+
 
 
