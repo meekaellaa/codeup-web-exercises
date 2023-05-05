bfc3d156c358ffd24b51b0ab745eb0e4c8e27d1d
Michaella Moreno <michaella.moreno@gmail.com>
2023-05-05 09:26:23 -0500

    completed warm-up.

--- js/warmup.js
+++ js/warmup.js
@@ -9,7 +9,6 @@
 //David's Solution:
 //Looks just like yours! Good Job!
 
-
 //Warm -Up 05/01/2023
 // Write a function that takes in a number and returns an array of numbers that are divisible within the argument. If the value provided is not a number, the function should return false.
 
@@ -30,7 +29,6 @@
 
 // david calls this a bucket function..
 
-
 // console.log(findFactors(6)); // returns [1,2,3,6];
 // findFactors(16); // returns [1,2,4,8,16];
 // findFactors(0); // returns [];
@@ -39,8 +37,6 @@
 // findFactors([54,72,144]); // returns false;
 // findFactors({value: 64}); // returns false;
 // findFactors(); // returns false;
-
-
 
 // Warm-up 05/02/2023
 // Write a function that takes in a string and counts the instances of the letter 'e' within the argument. The count should be case insensitive. If the string does not contain any 'e's it should return 0. If the argument is not a string it should return false.
@@ -126,10 +122,8 @@
 // console.log(takeString(['e', 'E'])); // returns false;
 // console.log(takeString()); // returns false;
 
-
 //Warm-Up 04/04/2023
 // Write a function named fizzBuzz that prints to the console the number 1 - 100. If the number is divisible by 3 print fizz instead of the number. If the number is divisible by 5 print buzz. If the number is divisible by both 3 and 5 print fizzBuzz.
-
 
 // function fizzBuzz(x){
 //     for(let x =1 ; x <=100; x++)
@@ -159,48 +153,33 @@
 // }
 //1. Start 1-100 to print in console
 //2.
-function fizzBuzz(){
+// function fizzBuzz(){
+//
+//     for (let i=1;i <= 100;i++){
+//        if(i % 3 === 0 && i % 5 === 0){
+//            console.log("fizzbuzz");
+//        } else if(i % 5 === 0){
+//             console.log("buzz")
+//        } else if(i % 3 === 0){
+//            console.log("fizz")
+//        }else{
+//            console.log(i);
+//        }
+//     }
+// }
+//
+// fizzBuzz();
 
-    for (let i=1;i <= 100;i++){
-       if(i % 3 === 0 && i % 5 === 0){
-           console.log("fizzbuzz");
-       } else if(i % 5 === 0){
-            console.log("buzz")
-       } else if(i % 3 === 0){
-           console.log("fizz")
-       }else{
-           console.log(i);
-       }
-    }
-}
+//Warm-Up 05/05/2023
+// Create a do-while loop that starts at 2, and displays the number squared on each line while the number is less than 1,000,000. Output should equal:
+// 2
+// 4
+// 16
+// 256
+// 65536
 
-fizzBuzz();
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+let i = 2;
+do {
+  console.log(i);
+  i *= i;
+} while (i < 100000);
